fdd3f940518972ea5a68704d1b884a88020d64f5
sowmik-sec <sowmik.sec@gmail.com>
2023-02-20 15:13:52 +0600

    showing full details of a meal

--- src/components/ShowOneMeal/ShowOneMeal.js
+++ src/components/ShowOneMeal/ShowOneMeal.js
@@ -6,6 +6,7 @@ const ShowOneMeal = () => {
   const { strInstructions, strMeal, strMealThumb, strCategory, strArea } =
     meal.meals[0];
   console.log(meal);
+  const vowel = ["a", "e", "i", "o", "u"];
   return (
     <div>
       <h1>Meal details</h1>
@@ -13,7 +14,10 @@ const ShowOneMeal = () => {
         <img src={strMealThumb} alt="" />
         <h2>{strMeal}</h2>
         <p>Category: {strCategory}</p>
-        <p>This is a {strArea} food.</p>
+        <p>
+          This is {vowel.includes(strArea[0].toLowerCase()) ? "an" : "a"}{" "}
+          {strArea} food.
+        </p>
         <h3>Recipe:</h3>
         <p>{strInstructions}</p>
       </div>
